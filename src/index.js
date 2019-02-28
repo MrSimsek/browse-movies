293e04e235f3858f1534d9661be6cabe4e20e91b
mrsimsek <denizsimsek93@gmail.com>
2019-02-28 15:14:30 +0300

    Modify Redux Tools checking code

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const allReducers = combineReducers({
 
 const allStoreEnhancers = compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
 );
 
 const store = createStore(
